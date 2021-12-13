f7d7d05b0d8d263db43afa3a95153789e8e4253d
Kusuka-Masako <79459969+Kusuka-Masako@users.noreply.github.com>
2021-12-13 19:05:49 +0700

    Praktikum 9

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -15,7 +15,7 @@ namespace ServiceReservasi
         string Pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi);
 
         [OperationContract]
-        string editPemesanan (string IDPemesanan, string NamaCustomer);
+        string editPemesanan (string IDPemesanan, string NamaCustomer, string No_telpon);
 
         [OperationContract]
         string deletePemesanan(string IDPemesanan);
@@ -63,7 +63,7 @@ namespace ServiceReservasi
         [DataMember]
         public int JumlahPemesanan { get; set; }
         [DataMember]
-        public string IDLokasi { get; set; }
+        public string Lokasi { get; set; }
     }
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
